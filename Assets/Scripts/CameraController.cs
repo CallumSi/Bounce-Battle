425f962298baf12a9a1be462dd645120cee54ed8
Callum Simpson <59879127+CallumSi@users.noreply.github.com>
2022-05-22 23:33:13 +0100

    Optimised camera controller class and fixed clamping

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,3 +1,5 @@
+
+   
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,15 +8,16 @@ public class CameraController : MonoBehaviour
 {
     //store the previous position of the mouse
     private Vector3 previousMousePosition;
+    //store the previous distance between the last two touches
     private float previousPinchDistance;
+    //store the current mouse position
+    private Vector3 currentMousePosition;
+    //store the direction vector between last two mouse/touch inputs
+    private Vector3 directionVector;
     //store the distance zoomed in or out
-    private float zoomDistance =-25;
-    private bool lockNegativeRotation=false;
-    private bool lockPositiveRotation = false;
+    private float zoomDistance = -25;
     void Start()
     {
-     
- 
         //set the inital position 
         transform.position = new Vector3(0, 5, 0);
         //rotate then rotate and scale outwards
@@ -25,117 +28,101 @@ public class CameraController : MonoBehaviour
 
     void Update()
     {
-       //if right click occours
-        if (Input.GetMouseButtonDown(1)|Input.touchCount>2)
+        //if right click occours
+        if (Input.GetMouseButtonDown(1))
         {
             //save the position of the mouse
             previousMousePosition = Camera.main.ScreenToViewportPoint(Input.mousePosition);
-            if (Input.touchCount > 2)
+            //if two finger input
+            if (Input.touchCount == 2)
             {
+                //get both the touches
                 Touch touch1 = Input.GetTouch(0);
                 Touch touch2 = Input.GetTouch(1);
+                //store the distance between the pinches 
                 previousPinchDistance = Vector2.Distance(touch1.position, touch2.position) / 100;
             }
-           
-
         }
         //if right click sill held
-        else if (Input.GetMouseButton(1)| Input.touchCount > 2)
+        else if (Input.GetMouseButton(1))
         {
-           
-            Vector3 currentMousePosition = Camera.main.ScreenToViewportPoint(Input.mousePosition);
-            //get the direction vector from the previous mouse  pos to new mouse pos
-            Vector3 directionVector = previousMousePosition - currentMousePosition;
-           
-           
-            
+            //Get the direction vector
+            UpdateDirectionVector();
+            //update the zoom distance
+            UpdateZoomDistance();
+            //clamp rotation
+            //if the angle is above 70 and you are trying to move up
+            if (transform.rotation.eulerAngles.x > 70f && directionVector.y > 0)
+            {
+                //set the rotation amount to 0
+                directionVector.y = 0;
+            }
+            //if the angle is below 10 and you are trying to move down
+            else if (transform.rotation.eulerAngles.x < 10f && directionVector.y < 0)
+            {
+                directionVector.y = 0;
+            }
             //position the camera at the centre of the base
             transform.position = new Vector3(0, 10, 0);
             //rotate around x axis
-            if (lockNegativeRotation==false && lockPositiveRotation==false)
-            {
-                transform.Rotate(new Vector3(1, 0, 0), directionVector.y * 180);
-            }
+            transform.Rotate(new Vector3(1, 0, 0), directionVector.y * 180);
             //rotate around y axis 
-          
             transform.Rotate(new Vector3(0, 1, 0), -directionVector.x * 180, Space.World);
-            
-           
-           //Check for  scroll wheel input
-            if (Input.GetAxis("Mouse ScrollWheel") != 0f)
-            {
-                //modify zoom distance
-                zoomDistance += (Input.GetAxis("Mouse ScrollWheel") * 10);
-                
-               
-                //Camera.main.nearClipPlane = -zoomDistance/3;
-            }
-            //Check for two finger input
-            if (Input.touchCount == 2)
-            {
-                Touch touch1 = Input.GetTouch(0);
-                Touch touch2 = Input.GetTouch(1);
-                float newPinchDistance = Vector2.Distance(touch1.position, touch2.position)/100;
-                Debug.Log(newPinchDistance);
-                if (newPinchDistance > previousPinchDistance && newPinchDistance>1)
-                {
-                    zoomDistance += newPinchDistance;
-                }
-                if (newPinchDistance < previousPinchDistance && newPinchDistance > 1)
-                {
-                    zoomDistance -= newPinchDistance;
-                }
-
-            }
-            //clamp the values
-            if (zoomDistance < -20)
-            {
-                zoomDistance = -20;
-
-            }
-            if (zoomDistance > -10)
-            {
-                zoomDistance = -10;
-            }
-
-        
-
-
             //translate in the z direction(outward)
             transform.Translate(new Vector3(0, 0, zoomDistance));
-            //clamp negative rotation
-            if (transform.position.y < 3)
-            {
-                lockNegativeRotation = true;
-                Debug.Log("rotation y locked");
-                transform.position = new Vector3(transform.position.x, 3, transform.position.z);
-            }
-            if (directionVector.y > 0)
-            {
-                lockNegativeRotation = false;
-                Debug.Log("rotation y unlocked");
-            }
-
-            //clamp negative rotation
-            if (transform.position.y > 28)
-            {
-                lockPositiveRotation = true;
-                Debug.Log("rotation y locked");
-                transform.position = new Vector3(transform.position.x, 28, transform.position.z);
-            }
-            if (directionVector.y < 0)
-            {
-                lockPositiveRotation = false;
-                Debug.Log("rotation y unlocked");
-            }
-
-
-            Debug.Log(directionVector.y);
             //update the mouse position
             previousMousePosition = currentMousePosition;
 
-            
         }
-        
+      
+    }
+    private void UpdateDirectionVector()
+    {
+        currentMousePosition = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        //get the direction vector from the previous mouse  pos to new mouse pos
+        directionVector = previousMousePosition - currentMousePosition;
+    }
+
+    private void UpdateZoomDistance()
+    {
+        //Check for  scroll wheel input
+        if (Input.GetAxis("Mouse ScrollWheel") != 0f)
+        {
+            //modify zoom distance
+            zoomDistance += (Input.GetAxis("Mouse ScrollWheel") * 10);
+            //modify the near clipping plane of the camera based on the zoom amount 
+            //Camera.main.nearClipPlane = -zoomDistance/3;
+
+        }
+        //Check for two finger input
+        if (Input.touchCount == 2)
+        {
+            //get the first and second touch 
+            Touch touch1 = Input.GetTouch(0);
+            Touch touch2 = Input.GetTouch(1);
+            //get this ditance between the two touches
+            float newPinchDistance = Vector2.Distance(touch1.position, touch2.position) / 100;
+            //if the distance between pinces is increasing
+            if (newPinchDistance > previousPinchDistance && newPinchDistance > 1)
+            {
+                zoomDistance += newPinchDistance;
+            }
+            //if the distance between pinces is decreasing 
+            if (newPinchDistance < previousPinchDistance && newPinchDistance > 1)
+            {
+                zoomDistance -= newPinchDistance;
+            }
+
+        }
+        //clamp the values
+        if (zoomDistance < -20)
+        {
+            zoomDistance = -20;
+
+        }
+        if (zoomDistance > -10)
+        {
+            zoomDistance = -10;
+        }
     }
 }
